27fd3eb794e829b593f3ea0996921c30ab611831
Jiaping Wang <wangjp9802@gmail.com>
2018-02-07 00:54:24 -0500

    matrix finished

--- hw2/matrix.c
+++ hw2/matrix.c
@@ -8,7 +8,6 @@
 int A[N][N];
 int B[N][N];
 int C[N][N];
-unsigned int seeds[N][N];
 int MAX_ROW_SUM = 0;
 pthread_mutex_t lock;
 
@@ -19,30 +18,41 @@ struct timeSeed{
 	int yLoc;
 };
 
+struct timeSeed seeds[N][N];
+
 void *assignNumber(void *args);
 void *multiply(void *args);
 void *maxRowSum(void *args);
-void showMatrix(int[][] Matrix);
+void showMatrix(int Matrix[N][N]);
 
 int main(int argc, char **argv) {
 	pthread_t threads[N][N];
-	struct timeSeed seeds[N][N];
+	//struct timeSeed seeds[N][N];
 	int aThread;
 	pthread_mutex_init(&lock, NULL);
-	for(int i = 0; i < N; i++)
-		for(int j = 0; j < N; j++)
-			for(int k = 0; k < 1; k++) {
-				if(k) {
-					seeds[i][j].seed1 = time(NULL);
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N; j++) {
+			for(int k = 0; k < 2; k++) {
+				if(k == 0) {
+					struct timeval currentTime;
+					gettimeofday(&currentTime, NULL);
+					seeds[i][j].seed1 = (currentTime.tv_usec * 7 / 11); 
+					sleep(0.001);
+					//printf("Test: %ld\n", seeds[i][j].seed1);
 				}
 				else {
-					seeds[i][j].seed2 = time(NULL);
+					struct timeval currentTime;
+					gettimeofday(&currentTime, NULL);
+					seeds[i][j].seed2 = currentTime.tv_usec * 11 / 7;
+					sleep(0.001);
 					seeds[i][j].xLoc = i;
 					seeds[i][j].yLoc = j;
 				}
 			}
+		}
+	}
 	
-	for(int i = 0; i < N; i++)
+	for(int i = 0; i < N; i++) {
 		for(int j = 0; j < N; j++) {		
 			aThread = pthread_create(&threads[i][j], NULL, &assignNumber, &seeds[i][j]);
 			if(aThread != 0) {
@@ -51,8 +61,9 @@ int main(int argc, char **argv) {
 				exit(1);
 			}
 		}
+	}
 
-	for(int i = 0; i < N; i++)
+	for(int i = 0; i < N; i++) {
 		for(int j = 0; j < N; j++) {	
 			aThread = pthread_join(threads[i][j], NULL);
 			if(aThread != 0) {
@@ -61,13 +72,14 @@ int main(int argc, char **argv) {
 				exit(1);
 			}
 		}
+	}
 
 	printf("Matrix A:\n");
 	showMatrix(A);
 	printf("Matrix B:\n");
 	showMatrix(B);
 
-	for(int i = 0; i < N; i++)
+	for(int i = 0; i < N; i++) {
 		for(int j = 0; j < N; j++) {		
 			aThread = pthread_create(&threads[i][j], NULL, &multiply, &seeds[i][j]);
 			if(aThread != 0) {
@@ -76,8 +88,9 @@ int main(int argc, char **argv) {
 				exit(1);
 			}
 		}
+	}
 
-	for(int i = 0; i < N; i++)
+	for(int i = 0; i < N; i++) {
 		for(int j = 0; j < N; j++) {	
 			aThread = pthread_join(threads[i][j], NULL);
 			if(aThread != 0) {
@@ -86,16 +99,18 @@ int main(int argc, char **argv) {
 				exit(1);
 			}
 		}
+	}
 
 	printf("Matrix C:\n");
 	showMatrix(C);
 
 	for(int j = 0; j < N; j++) {		
-			aThread = pthread_create(&threads[j][0], NULL, &multiply, &seeds[j][0]);
+			aThread = pthread_create(&threads[j][0], NULL, &maxRowSum, &seeds[j][0]);
 			if(aThread != 0) {
 				printf("Fail to create thread! Program exit in one second!\n");
 				sleep(1);
 				exit(1);
+			}
 	}
 
 	for(int j = 0; j < N; j++) {	
@@ -104,6 +119,7 @@ int main(int argc, char **argv) {
 				printf("Fail to join thread! Program exit in one second!\n");
 				sleep(1);
 				exit(1);
+			}
 	}
 
 	printf("MAX_ROW_SUM is: %d\n", MAX_ROW_SUM);
@@ -112,43 +128,46 @@ int main(int argc, char **argv) {
 }
 
 void *assignNumber(void *args) {
-	struct timeSeed *seeds = args;
-	int i = args.xLoc;
-	int j = args.yLoc;
-	unsigned int seed1 = args.seed1;
-	unsigned int seed2 = args.seed2;
+	struct timeSeed *seed = args;
+	int i = seed->xLoc;
+	int j = seed->yLoc;
+	unsigned int seed1 = seed->seed1;
+	unsigned int seed2 = seed->seed2;
 	A[i][j] = rand_r(&seed1) % 10;
 	B[i][j] = rand_r(&seed2) % 10;
 }
 
 void *multiply(void *args) {
-	struct timeSeed *seeds = args;
-	int i = args.xLoc;
-	int j = args.yLoc;
+	struct timeSeed *seed = args;
+	int i = seed->xLoc;
+	int j = seed->yLoc;
 	C[i][j] = 0;
-	for(int x = 0; x < N; x++) {
+	int x;
+	for(x = 0; x < N; x++) {
 		C[i][j] += A[i][x] * B[x][j];
 	}
-	return;
+	//return;
 }
 
 void *maxRowSum(void *args) {
-	struct timeSeed *seeds = args;
-	int i = args.xLoc;
-	int j = args.yLoc;
-	int sum = 0
+	struct timeSeed *seed = args;
+	int i = seed->xLoc;
+	int j = seed->yLoc;
+	int sum = 0;
 	for(int x = 0; x < N; x++) {
 		sum += C[i][x];
 	}
+	//printf("Row %d has a sum of %d\n", i, sum);
 	pthread_mutex_lock(&lock);
+	printf("Row %d has a sum of %d\n", i, sum);
 	if(sum >= MAX_ROW_SUM) {
 		MAX_ROW_SUM = sum;
 	}
 	pthread_mutex_unlock(&lock);
-	return;
+	//return;
 }
 
-void showMatrix(int[][] Matrix){
+void showMatrix(int Matrix[N][N]){
 	for(int i = 0; i < N; i++) {
 		for(int j = 0; j < N; j++) {
 			printf("%d ", Matrix[i][j]);
