a627a90e4a4367a72b21717efb4871ded387e9ab
Jiaping_Wang <nflshc2020@gmail.com>
2018-03-18 22:36:17 -0400

    tre

--- hw4/userthread.cpp
+++ hw4/userthread.cpp
@@ -452,6 +452,7 @@ void my_scheduler() {
 		}
 		else if (current_thread->status == YIELD){
 			if (schedule_policy == _FIFO) {
+				printf("I am fucking here\n");
 				ready_FIFO.push(activeID);
 				nextID = choose_next_thread_FIFO();
 				if (nextID == NOT_FOUND) {
