8b43050f4bb6c3462fe77709f513c53af9afb28d
Jiaping_Wang <nflshc2020@gmail.com>
2018-03-18 21:00:41 -0400

    show respect

--- hw4/userthread.cpp
+++ hw4/userthread.cpp
@@ -128,8 +128,8 @@ int thread_join(int tid) {
 		current_thread->context = save_context;
 		current_thread->wait_tid = tid;
 	}
-	swapcontext(&save_context, &scheduler_context);
 	printf("%d\n", current_thread->tid);
+	swapcontext(&save_context, &scheduler_context);
 	return EXIT_SUCCESS;
 }
 
