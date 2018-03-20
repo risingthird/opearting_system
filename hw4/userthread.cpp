87cd868f6553a47c5469c96f059890f3284a0f7f
Jiaping_Wang <nflshc2020@gmail.com>
2018-03-20 15:19:12 -0400

    v

--- hw4/userthread.cpp
+++ hw4/userthread.cpp
@@ -513,14 +513,17 @@ void my_scheduler() {
 					ready_SJF.push(temp);
 					current_thread->suspended_queue.pop();
 				}
-				printf("I am fucking here with current tid of %d\n", current_active->tid);
+
 				thread_PRI_SJF_FIFO* temp3 = NULL;
 				temp3 = choose_next_thread_SJF();
 				if (temp3 == NULL) {
 					makecontext(&scheduler_context, my_scheduler, 0);
+					printf("I am fucking here with current tid of %d\n", current_active->tid);
 					current_active = NULL;
+					
 					swapcontext(&scheduler_context, &(main_thread->context));
 				}
+
 				nextID = temp3->id;
 				//printf("The next to arrive is %d\n", nextID);
 				delete(temp3);
