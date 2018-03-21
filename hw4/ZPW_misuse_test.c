624de15779deced68a705fdd84fdf1901540d910
Jiaping_Wang <nflshc2020@gmail.com>
2018-03-20 23:06:50 -0400

    test

--- hw4/ZPW_misuse_test.c
+++ hw4/ZPW_misuse_test.c
@@ -42,8 +42,11 @@ int main(void)
     }
     for (int i = 0; i < THREAD_NUMBER; i++)
     {
-        if (thread_join(tids[i]) == FAIL)
+        if (thread_join(tids[i]) == FAIL) {
+
             exit(EXIT_FAILURE);
+        }
+        printf("I am with thread id %d\n", i);
     }
     printf("I am here\n");
     if (thread_libterminate() == FAIL)
