424750a449c492ece1cb1ae37dfbcadf8f25def8
Jiaping_Wang <nflshc2020@gmail.com>
2018-04-03 14:37:07 -0400

    try

--- hw5/mem.c
+++ hw5/mem.c
@@ -35,7 +35,7 @@ int Mem_Init(long sizeOfRegion) {
 		return RETURN_WITH_ERROR;
 	}
 
-	long to_alloc = round_to(sizeOfRegion, BLOCK_SIZE) * BLOCK_HEADER + sizeOfRegion;
+	long to_alloc = round_to(sizeOfRegion, BLOCK_SIZE) / BLOCK_SIZE * BLOCK_HEADER + sizeOfRegion;
 	long to_alloc_with_global = round_to(to_alloc + GLOBAL_SIZE, getpagesize());
 
 	if ( (global_head = mmap(NULL, to_alloc_with_global, PROT_READ | PROT_WRITE,  MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)) == MAP_FAILED) {
